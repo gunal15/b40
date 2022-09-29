2f96bc47c0e27b851c73f1a154d9770c62cd98ee
gunal15 <gunalnandakumar@gmail.com>
2022-09-29 11:47:44 +0530

    task 5 added

--- html js/rest countries1.js
+++ html js/rest countries1.js
@@ -4,9 +4,20 @@ request.send();
 request.onload=function(){
     var result=JSON.parse(request.response);
     
+    //var res=result.filter((ele)=>ele.region==="Europe").map((ele)=>ele.name);
+    //console.log(res);
+    var sum=0;
+
+    for(var i=0;i<result.length;i++){
+      sum+=result[i].population;
+    }
+    console.log(sum);
+
+    var res1=result.reduce((acc,cv)=>acc+cv.population,0);
+    console.log(res1);
 //   code here
-for(var i=0;i<result.length;i++){
-  console.log(result[i].name,result[i].population,result[i].region,result[i].subregion)
+//for(var i=0;i<result.length;i++){
+  //console.log(result[i].name,result[i].population,result[i].region,result[i].subregion)
    
-}
+//}
 };
